5b7f2e6b3b359835ef249dfc526d081a450fd8a9
abdullahhaktan <abdullahhaktan@18@gmail.com>
2025-02-26 14:50:45 +0300

    Projeyi ilk kez gönderiyorum

---
+++ BusinessLayer/Concrete/WriterMessageManager.cs
@@ -0,0 +1,63 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class WriterMessageManager : IWriterMessageService
+    {
+        IWriterMessageDal _writerMessageDal;
+
+        public WriterMessageManager(IWriterMessageDal writerMessageDal)
+        {
+            _writerMessageDal = writerMessageDal;
+        }
+
+        public List<WriterMessage> GetListReceiverMessage(string mail)
+        {
+            return _writerMessageDal.GetbyFilter(item => item.Receiver == mail);
+        }
+
+
+        public List<WriterMessage> GetListSenderMessages(string mail)
+        {
+            return _writerMessageDal.GetbyFilter(item => item.Sender == mail);
+        }
+
+
+        public void TAdd(WriterMessage t)
+        {
+            _writerMessageDal.Insert(t);
+        }
+
+        public void TDelete(WriterMessage t)
+        {
+            _writerMessageDal.Delete(t);
+        }
+
+        public WriterMessage TGetByID(int id)
+        {
+            return _writerMessageDal.GetByID(id);
+        }
+
+        public List<WriterMessage> TGetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<WriterMessage> TGetListbyFilter()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(WriterMessage t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Core_Proje/Areas/Writer/Controllers/MessageController.cs
@@ -0,0 +1,97 @@
+﻿
+using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Proje.Areas.Writer.Controllers
+{
+    [Area("Writer")]
+    [Route("Writer/Message")]
+
+    public class MessageController : Controller
+    {
+        WriterMessageManager writerMessageManager = new WriterMessageManager(new EfWriterMessageDal());
+
+        private readonly UserManager<WriterUser> _userManager;
+
+        public MessageController(UserManager<WriterUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [Route("")]
+        [Route("ReceiverMessage")]
+        public async Task<IActionResult> ReceiverMessage(string mail)
+        {
+            var userValues = await _userManager.FindByNameAsync(User.Identity.Name);
+            mail = userValues.Email;
+            var messageList = writerMessageManager.GetListReceiverMessage(mail);
+            return View(messageList);
+        }
+
+        [Route("")]
+        [Route("SenderMessage")]
+        public async Task<IActionResult> SenderMessage(string mail)
+        {
+            var userValues = await _userManager.FindByNameAsync(User.Identity.Name);
+            mail = userValues.Email;
+            var messageList = writerMessageManager.GetListSenderMessages(mail);
+            return View(messageList);
+        }
+
+        [Route("MessageDetails/{id}")]
+        public IActionResult MessageDetails(int id)
+        {
+            WriterMessage writerMessage = writerMessageManager.TGetByID(id);
+            return View(writerMessage);
+        }
+
+        [Route("ReceiverMessageDetails/{id}")]
+        public IActionResult ReceiverMessageDetails(int id)
+        {
+            WriterMessage writerMessage = writerMessageManager.TGetByID(id);
+            return View(writerMessage);
+        }
+
+        [HttpGet]
+        [Route("")]
+        [Route("SendMessage")]
+        public IActionResult SendMessage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("")]
+        [Route("SendMessage")]
+        public async Task<IActionResult> SendMessage(WriterMessage p)
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+
+            string mail = values.Email;
+
+            string nameSurname = values.Name + " " + values.Surname;
+
+            p.Date = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+
+            p.Sender = mail;
+            p.SenderName = nameSurname;
+            Context c = new Context();
+
+            var usernamesurname = c.Users.Where(item => item.Email == p.Receiver).Select(y => y.Name + " " + y.Surname).FirstOrDefault();
+            p.ReceiverName = usernamesurname;
+
+            writerMessageManager.TAdd(p);
+
+            return RedirectToAction("SenderMessage");
+
+        }
+    }
+}
